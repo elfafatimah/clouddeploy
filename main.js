f99022812e785058461fd75d2eb0f972fa5b0846
ARR <arr@pop-os.localdomain>
2020-12-18 10:10:53 +0700

    sayakuat

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var API_URL = 'https://rs3iu0fxg3.execute-api.us-east-1.amazonaws.com/dev/data-mhs';
+var API_URL = 'https://zhaki5ndu4.execute-api.us-east-1.amazonaws.com/proakhirdeploy/datamhs';
+
 
 $(function (){
   var $table_data = $('#table_data');
@@ -11,13 +12,13 @@ $(function (){
     dataType: 'json',
     success: function(table_data) {
       var event_data='';
-      $.each(table_data, function(i, data_mhs) {
+      $.each(table_data, function(i, datamhs) {
                  event_data += '<tr>';
-                 event_data += '<td>'+data_mhs.nim+'</td>';
-                 event_data += '<td>'+data_mhs.nama+'</td>';
+                 event_data += '<td>'+datamhs.nim+'</td>';
+                 event_data += '<td>'+datamhs.nama+'</td>';
                  event_data += '</tr>';
       });
-      $("#table_mhs").append(event_data);
+      $("#datamahasiswa").append(event_data);
     },
     error: function(){
       alert('error fetching');
@@ -28,45 +29,3 @@ $(function (){
 
 
 
-//
-// $(function (ss){
-//   var $add-pengajar = $('#add-pengajar');
-//
-//   $.ajax({
-//     type:  'POST',
-//     url: 'https://358wt7ug67.execute-api.us-east-1.amazonaws.com/cors1/authors/',
-//     success: function(add-pengajars) {
-//       $.each(add-pengajars, function(i, authors) {
-//         $pengajars.append('<li>firstName: '+ authors.lastName +', lastName: '+ authors.firstName +'</li>');
-//       });
-//     },
-//     error: function(){
-//       alert('error fetching');
-//     }
-//   });
-// });
-//
-//
-//
-//
-
-
-
-
-//
-// $('#add-pengajar').on('click', function(){
-//   //
-//   // var order = {
-//   //   first: $first.val();
-//   //   last: $last.val();
-//   // };
-//   $.ajax({
-//       type: 'POST',
-//       url: API_URL,
-//       dataType: 'json',
-//       data : authors,
-//       success: function() {
-//         // $table_data.append('<li>Name: '+ authors.lastName +', '+ authors.firstName +'</li>');
-//       },
-//
-//   });
